797b2003d501732279f5dc841bc729bf7128d40c
mgr-pc <mgr9525@qq.com>
2018-07-20 19:40:17 +0800

    first init project

---
+++ src/main/kotlin/RuisMain.kt
@@ -0,0 +1,14 @@
+
+import com.kotlint.stdui.mgr.*
+import com.kotlint.stdui.kui.*
+
+class MainWindow:KUIBaseWindow("main.json"){
+
+}
+
+fun main(args:Array<String>){
+    val mgr=getWindowManager()
+    mgr.create(0)
+    MainWindow().create()
+    mgr.update()
+}
